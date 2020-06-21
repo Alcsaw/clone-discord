02162254975c2afb478ba9f84eac801bd606a22b
Alcsaw <alcsaw@hotmail.com>
2020-06-20 21:39:58 -0300

    create ServerList (left side bar)

---
+++ src/components/ServerList/index.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import ServerButton from '../ServerButton';
+
+import { Container, Separator } from './styles';
+
+const ServerList: React.FC = () => {
+    return (
+        <Container>
+            <ServerButton isHome />
+            <Separator />
+
+            <ServerButton />
+
+            <ServerButton hasNotifications mentions={1} />
+
+            <ServerButton hasNotifications />
+
+            <ServerButton />
+
+            <ServerButton hasNotifications mentions={3} />
+
+        </Container>
+    )
+};
+
+export default ServerList;
