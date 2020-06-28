4c23240bd8e7c5bbf052be0377a7c9e14f72f0e5
Alcsaw <alcsaw@hotmail.com>
2020-06-28 13:31:03 -0300

    Adds TopMenu component

--- src/components/Layout/styles.ts
+++ src/components/Layout/styles.ts
@@ -12,10 +12,10 @@ export const Grid = styled.div`
     display: grid;
 
     grid-template-columns: 71px 240px auto 240px;
-    grid-template-rows: 46px auto 52px;
+    grid-template-rows: 48px auto 52px;
 
     grid-template-areas:
-        'SL SN CI CI'
+        'SL SN CI TM'
         'SL CL CD UL'
         'SL UI CD UL';
 
