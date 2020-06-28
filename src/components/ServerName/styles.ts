92968aae42d3b8d86b038b60262ab24bce767f24
Alcsaw <alcsaw@hotmail.com>
2020-06-28 08:51:13 -0300

    Fix box-shadow in ServerName component

--- src/components/ServerName/styles.ts
+++ src/components/ServerName/styles.ts
@@ -3,15 +3,16 @@ import { ExpandMore } from 'styled-icons/material';
 
 export const Container = styled.div`
     display: flex;
-    justify-content: space-between;
     align-items: center;
+    justify-content: space-between;
 
     padding: 0 11px 0 16px;
 
     background-color: var(--secondary);
     box-shadow: rgba(0, 0, 0, 0.2) 0px 1px 0px 0px;
-    z-index: 2;
+    z-index: 3;
 `;
+
 export const Title = styled.h1`
     font-size: 16px;
     font-weight: bold;
